41ae25f86c84f2e84aefb7b1c53c0fec32d2c570
Christian Marlats <marlats.christian@gmail.com>
2026-03-14 16:09:28 +0100

    feat: rendere header e footer globali con esclusione area admin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,13 @@
-import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import CategoryGrid from '@/components/CategoryGrid';
 import FeaturedProducts from '@/components/FeaturedProducts';
-import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-
-      <main className="flex-grow">
-        <Hero />
-        <CategoryGrid />
-        <FeaturedProducts />
-      </main>
-
-      <Footer />
-    </div>
+    <>
+      <Hero />
+      <CategoryGrid />
+      <FeaturedProducts />
+    </>
   );
 }
